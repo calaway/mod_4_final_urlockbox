c0c3a20943aca5efb68a9d1fd503f397e2f0303f
Calaway <calaway@users.noreply.github.com>
2017-01-09 13:45:17 -0700

    Extract Hot Reads logic out to service.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,6 +2,5 @@ class LinksController < ApplicationController
   before_action :authenticate!
 
   def index
-    @hot_links = Link.hot
   end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,12 +1,4 @@
 class Link < ApplicationRecord
   validates :url, :url => true
   belongs_to :user
-
-  scope :hot, -> {
-    select('links.url as url')
-      .joins('join reads on reads.link_id = links.id')
-      .where('reads.created_at > ?', Time.now - 1.day)
-      .group("links.url")
-      .order('count("reads".id) DESC').limit(10)
-  }
 end
